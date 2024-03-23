739f989743dfaae6c4baaf338e7fbb479cedbe3d
Ehtisham86 <ehtisham010101@gmail.com>
2024-03-24 00:36:26 +0500

    added a console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Wellcome to the Number Guessing Game!");
 const number = await inquirer.prompt([
     {
         message: "Please guess a number between 1-6:",
--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
 import inquirer from "inquirer";
 
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+console.log("Wellcome to the Number Guessing Game!");
+
 const number = await inquirer.prompt([
   {
     message: "Please guess a number between 1-6:",
