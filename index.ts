1b6a2669d04131e246a809da76baba59b8d180a0
Ehtisham86 <ehtisham010101@gmail.com>
2024-03-24 00:27:48 +0500

    Push my number guessing game to github

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const number = await inquirer.prompt([
+  {
+    message: "Please guess a number between 1-6:",
+    type: "number",
+    name: "userGuessedNumber",
+  },
+]);
+
+if (number.userGuessedNumber === randomNumber) {
+  console.log("Congratulations, you guessed correctly.");
+} else {
+  console.log("Game Over, you guessed wrong number.");
+}
